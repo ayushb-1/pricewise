016163f75edb77d708985b633d24c02ac82c126d
ayush bhatia <101511675+ayushb-1@users.noreply.github.com>
2024-08-11 15:12:41 +0530

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
 
     try {
@@ -34,7 +38,7 @@ export async function GET() {
             
     
             const updatedProduct = await Product.findOneAndUpdate(
-                {url: scrapedProduct.url},
+                {url: product.url},
                 product,
             )
             // 2. CHECK EACH PRODUCT STATUS AND SEND EMAIL ACCORDINGLY
